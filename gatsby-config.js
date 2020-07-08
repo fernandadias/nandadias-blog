f9e8d332b91cedf0243eb12d7118e4774e5d533d
Nanda Dias <fernanda.dias@konduto.com>
2020-07-08 14:42:16 -0300

    Cleanup and add Avatar component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Nanda Dias`,
+    description: `Designer que também gosta de papos sobre front-end, back-end, dados, música e produto`,
+    author: `@nandadiasmusic`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,10 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#6356FE`,
+        theme_color: `#6356FE`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/nanda-dias-icon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
